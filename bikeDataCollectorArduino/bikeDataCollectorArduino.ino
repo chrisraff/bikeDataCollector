67100986f1c28c39d4fae93537ae38bf8f4d8e4e
Kevin Zheng <kyzheng@umass.edu>
2019-06-06 11:08:44 -0400

    cleaned up and reformatted code

--- bikeDataCollectorArduino/bikeDataCollectorArduino.ino
+++ bikeDataCollectorArduino/bikeDataCollectorArduino.ino
@@ -1,59 +1,40 @@
-/* 
- *  SD card configuration: MKR MEM SHIELD or d1 mini sd shield
- */
-
 #include <Streaming.h>
-//#include <SPI.h> // needed for esp32, not for others
 #include <SD.h>
 #include <WiFiNINA.h>
 #include <ArduinoLowPower.h>
 #include "credentials.h"
 
 // sd variables
-const int chipSelect = 4; // 4 for most things, 5 for esp32
+const int chipSelect = 4;  // 4 for most things, 5 for esp32
 bool sdBegan = false;
 
-// -------
+// initialize other variables
+bool firstRead = true;  // for discarding first serial values (most likely bad data)
+bool newData = false;  // for determining whether an upload is needed
+unsigned long lastSerialMillis = 0;  // for recording time of last serial data input
+const int serialTimeout = 5000;  // milliseconds of no data before timeout
 
-bool firstRead = true;
-
-bool newData = false;
-
-unsigned long lastSerialMillis = 0;
-const int serialTimeout = 5000;
-
-//unsigned long lightOffMillis = 0;
 
 void setup() {
   Serial.begin(9600);
-
   Serial1.begin(9600);
 
-  sdBegan = SD.begin(chipSelect);
+  sdBegan = SD.begin(chipSelect);  // determine the presence of SD card
 
   pinMode(LED_BUILTIN, OUTPUT);
 
-//  pinMode(13, INPUT);
-
-  // write to SD card
+  // write to SD card if SD card exists
   if (sdBegan) {
-      Serial << "SD card initialized" << endl;
-    
-//      lightOffMillis = millis() + 1000;
-    
-      File dataFile = SD.open("BikeData.txt", FILE_WRITE);
-
-      if (dataFile) {
-        dataFile << "Arduino turned on" << endl;
-
-        dataFile.close();
-      }
-  } else
-      Serial << "SD card failed to initialize" << endl;
-
+    Serial << "SD card initialized" << endl;
+    File dataFile = SD.open("BikeData.txt", FILE_WRITE);  
+    if (dataFile) {
+      dataFile << "Arduino turned on" << endl;
+      dataFile.close();
+    }
+  } 
+  else Serial << "SD card failed to initialize" << endl;
   lastSerialMillis = millis();
-
-  LowPower.attachInterruptWakeup(5, wakeMeUpInside, CHANGE); // digitalPinToInterrupt(5)?
+  LowPower.attachInterruptWakeup(5, wakeMeUpInside, CHANGE); // digitalPinToInterrupt(5)?  // set attachInterruptWakeup pin
 }
 
 void loop() {
@@ -65,19 +46,15 @@ void loop() {
       Serial << "Debug received " << Serial.readString() << endl;
     }
   }
-  
+
   // handle input
   if (Serial1.available()) {
     lastSerialMillis = millis();
-    
-    if (firstRead) {
+
+    if (firstRead) {  // discard first line of data
       Serial << "first read" << endl;
-      
       Serial << Serial1.readStringUntil('\n') << endl;
       firstRead = false;
-
-//      lightOffMillis = millis() + 500;
-
       return;
     }
 
@@ -95,37 +72,32 @@ void loop() {
     float acceleration = Serial1.parseFloat();
     String flags = Serial1.readStringUntil('\n');
 
-    
+
     // write to SD card
     if (sdBegan) {
       File dataFile = SD.open("BikeData.txt", FILE_WRITE);
-
       if (dataFile) {
         dataFile << millis() << ": " << ampHours << ", " << volts << ", "
-        << amps << ", " << speed << ", " << distance << ", " << degreesC << ", "
-        << rpm << ", " << humanWatts << ", " << nmRiderTorque << ", "
-        << throttleIn << ", " << throttleOut << ", " << acceleration << ", "
-        << flags << endl;
+                 << amps << ", " << speed << ", " << distance << ", " << degreesC << ", "
+                 << rpm << ", " << humanWatts << ", " << nmRiderTorque << ", "
+                 << throttleIn << ", " << throttleOut << ", " << acceleration << ", "
+                 << flags << endl;
 
         dataFile.close();
 
         newData = true;
-
-//        lightOffMillis = millis() + 100;
       }
-      else
+      else 
         Serial << "Couldn't write to file" << endl;
     }
   }
 
-  // if the bike stopped giving data, try to upload
+  // if the bike stopped giving data (turns off), try to upload
   if (millis() - lastSerialMillis >= serialTimeout && newData) {
-    firstRead = true;
+    firstRead = true;  # reset firstRead
     
     Serial << "Attempting to connect" << endl;
-    
     boolean connected = connect(ssid, pass, 10);
-
     if (!connected) {
       Serial << "no connection, sleeping" << endl;
       // low power mode and try again later
@@ -135,7 +107,7 @@ void loop() {
     }
     else
     {
-      // we have a wifi connection, attempt to upload the file
+      // wifi connected, attempt to upload the file
       Serial << "uploading file" << endl;
       File dataFile = SD.open("BikeData.txt", FILE_READ);
 
@@ -143,32 +115,27 @@ void loop() {
       client.connect("none.cs.umass.edu", 8099);
 
       // what if the server was down?
-  
+
       if (dataFile) {
         // upload the file
-        char data[1024];
+        char data[1024];  // make 1 MB packet to send
 
         boolean fileHasEnded = false;
-    
+
         while (!fileHasEnded) {
           int i = 0;
-          
           for (i = 0; i < 1024; i++) {
             int nextChar = dataFile.read();
-      
             if (nextChar == -1) {
               fileHasEnded = true;
               break;
             }
             data[i] = (char) nextChar;
           }
-    
           client << String(data).substring(0, i);
-    
           delay(250);
         }
         client << "<END>";
-
         Serial << "sent file, waiting for acknowledgement" << endl;
 
         // wait for acknowledgement
@@ -176,20 +143,17 @@ void loop() {
         while (millis() - startMillis < 20000 && !client.available()) {
           yield;
         }
-
         if (client.available()) {
           String ack = client.readString();
           Serial << "got: " << ack << endl;
-
+          
           if (ack.equals("great")) {
             Serial << "great, acknowledged" << endl;
             // the file successfully uploaded
             newData = false;
           }
         }
-        
         client.stop();
-     
         dataFile.close();
       }
     }
@@ -202,27 +166,12 @@ void loop() {
       Serial << "woke up (will I see this?)" << endl;
       return;
     }
-    else
-    {
+    else {
       Serial << "going to sleep forever bye" << endl;
-      LowPower.sleep(); // interrupt does not wake up from this sleep
+      LowPower.sleep();  // interrupt does not wake up from this sleep
       Serial << "do I wake up here?" << endl;
     }
-
-//    SD.remove("BikeData.txt");
   }
-
-//  if (millis() >= lightOffMillis) {
-////    digitalWrite(LED_BUILTIN, HIGH);
-////    
-////    if (millis() >= lightOffMillis + 1000) {
-////      lightOffMillis = millis() + 10;
-////      digitalWrite(LED_BUILTIN, LOW);
-////    }
-////  } else {
-////    digitalWrite(LED_BUILTIN, LOW);
-//
-//  }
 }
 
 
@@ -233,18 +182,17 @@ void wakeMeUpInside() {
 
 
 boolean connect(char* ssid, char* password, int maxAttempts) {
-  
-  byte t=0;
+  byte t = 0;
   int status = WiFi.status();
-  while ( ( status != WL_CONNECTED) || (WiFi.RSSI() <= -90) || (WiFi.RSSI() ==0) ) {  
-   // attempt to connect to WiFi network:
-   status = WiFi.begin(ssid, password);     // Connect to network.
-   delay(4000);                                // wait 4 seconds for connection:
-   if( t++ > maxAttempts){ // try-counter max
-    Serial1.readString(); // clear buffer
-    WiFi.end();
-    return false;
-   }
+  while ( ( status != WL_CONNECTED) || (WiFi.RSSI() <= -90) || (WiFi.RSSI() == 0) ) {
+    // attempt to connect to WiFi network:
+    status = WiFi.begin(ssid, password);  // Connect to network.
+    delay(4000);  // wait 4 seconds for connection:
+    if ( t++ > maxAttempts) {  // try-counter max
+      Serial1.readString();  // clear buffer
+      WiFi.end();
+      return false;
+    }
   }
   Serial1.readString();
   return true;
